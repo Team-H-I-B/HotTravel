6dd5bff1adfad7247faea956fe4721afeb0ba9f8
YIJIHO <yjh9099@gmail.com>
2024-01-08 02:25:46 +0900

    Final Test

--- src/main/java/com/hufs/Domain/Post/PostController.java
+++ src/main/java/com/hufs/Domain/Post/PostController.java
@@ -2,15 +2,6 @@ package com.hufs.Domain.Post;
 
 public class PostController {
     //sample
-    //test
-    //test2
-    //test3
-    //test4
-    //test5
-    //test6
-    //test7
-    //test8
-    //test9
-    //test10
+    //Final Test
 }
 
